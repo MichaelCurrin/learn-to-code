f58aabae47a23bf1daed2cb13b6419abfaa60480
MichaelCurrin <michael.currin@gmail.com>
2018-02-15 19:09:09 +0200

    feature: Create basics.c

---
+++ c/basics.c
@@ -0,0 +1,22 @@
+/*
+ * Basics in C.
+ *
+ * The line below is preprocessor comment which tells the compile to include
+ * the header file.
+ */
+#include <stdio.h>
+
+
+// Global variables.
+int a, b;
+
+
+void main()
+{
+    printf("Hello World\n");
+
+    printf("%c", 'H');
+
+    // Local variable.
+    float a;
+}
