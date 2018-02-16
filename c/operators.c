a87c3d2f9aa526540f23f5c79f264132744a95bf
MichaelCurrin <michael.currin@gmail.com>
2018-02-16 12:49:06 +0200

    refactor: Update operators.c

--- c/operators.c
+++ c/operators.c
@@ -4,17 +4,20 @@
 #include <stdio.h>
 
 
-void main() {
-    // Arithmetic.
-
-    int a = 3+2;
-    int b = 3-2;
+void arithmetic_operators() {
+    // Spaces are optional.
+    int a = 3 + 2;
+    int b = 3 - 2;
     int c = 3*2;
     int e = 3/2;
     int f = 3 % 2;
+}
 
 
+void increment_decrement_operators() {
     // Increment and decrement operators.
+    int a = 1;
+    int b = 2;
     a++;
     b--;
     printf("%d\n", a);
@@ -26,59 +29,83 @@ void main() {
     a = 3;
     b = ++a +6;
     // => a=4 b = 9
+}
 
 
-    // Relational operators.
+void relational_operators(){
     // See:
     //     https://www.tutorialspoint.com/cprogramming/c_relational_operators.htm
-    a = 21;
-    b = 10;
-    if( a == b ) {
+
+    // There is no boolean type in C, but true evaluates as 1 and false as 0.
+    int a = 21;
+    int b = 10;
+
+    int c;
+    if (a > b) {
+        c = 1;
+    }
+    else {
+        c = 0;
+    }
+
+    // Equivalent to above.
+    c = (a > b);
+
+
+    if(a == b) {
        printf("Line 1 - a is equal to b\n" );
     } else {
        printf("Line 1 - a is not equal to b\n" );
     }
 
-    if( a != b ) {
+    if(a != b) {
        printf("Line 1 - a is equal to b\n" );
     } else {
        printf("Line 1 - a is not equal to b\n" );
     }
 
 
-    if ( a < b ) {
+    if (a < b) {
        printf("Line 2 - a is less than b\n" );
     } else {
        printf("Line 2 - a is not less than b\n" );
     }
 
-    if ( a >= b ) {
+    if (a >= b) {
        printf("Line 3 - a is greater than b\n" );
     } else {
        printf("Line 3 - a is not greater than b\n" );
     }
+}
 
 
-    // Logical operators.
+void logic_operators() {
+    int a = 21;
+    int b = 10;
+
     // AND
-    if ( ( a >= b ) && ( a == 21 ) ) {
+    if((a >= b) && (a == 21)) {
        printf("Line 1 - a is greater b and a equal to 21\n" );
     } else {
        printf("Line 1 - a is not greater b or a not equal to 2\n" );
     }
     // OR
-    if ( ( a >= b ) || ( a == 21 ) ) {
+    if((a >= b) || (a == 21)) {
        printf("Line 1 - a is greater b or a is equal to 21\n" );
     } else {
        printf("Line 1 - a is not greater a or b is equal to 2\n" );
     }
+}
 
-    // Casting operators.
+
+void casting_operators() {
     int sum = 100;
     (float) sum;
     (int) sum;
+}
 
-    // Bitwise operators.
+
+void bitwise_operators() {
     /*
      * See:
      *    https://www.tutorialspoint.com/cprogramming/c_operators.htm
@@ -96,8 +123,10 @@ void main() {
     int D = 60 ^ 13;
     int E = 60 << 13;
     float F = 60 >> 13;
+}
 
-    // Assignment operators.
+
+void assignment_operators() {
     int assign = 1;
     assign += 1;
     assign -= 1;
@@ -109,4 +138,15 @@ void main() {
     assign &= 1;
     assign |= 1;
     assign ^= 1;
+}
+
+
+void main() {
+    arithmetic_operators();
+    increment_decrement_operators();
+    relational_operators();
+    logic_operators();
+    casting_operators();
+    bitwise_operators();
+    assignment_operators();
 }
