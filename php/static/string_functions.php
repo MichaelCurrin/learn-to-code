543628dd28a0f60bc84967e5f7086473c2421914
MichaelCurrin <michael.currin@gmail.com>
2018-03-02 20:23:44 +0200

    style: Indent code in scopes.php, string_functions.php and variables.php

--- php/static/string_functions.php
+++ php/static/string_functions.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <?php
+        echo "<h1>String functions</h1>";
 
-<?php
-
-echo "<h1>String functions</h1>";
-
-$t = "My Test String";
-echo "t: $t<br>";
-echo "strlen: " . strlen($t) . "<br>";
-echo "str_word_count: " . str_word_count($t) . "<br>";
-echo "strrev: " . strrev($t) . "<br>";
-echo "strpos: " . strpos($t, "Test");
-echo "str_replace: " . str_replace("new", "Test", $t);
-
-?>
-
+        $t = "My Test String";
+        echo "t: $t<br>";
+        echo "strlen: " . strlen($t) . "<br>";
+        echo "str_word_count: " . str_word_count($t) . "<br>";
+        echo "strrev: " . strrev($t) . "<br>";
+        echo "strpos: " . strpos($t, "Test");
+        echo "str_replace: " . str_replace("new", "Test", $t);
+    ?>
 </body>
 </html>
-
--- php/static/variables.php
+++ php/static/variables.php
@@ -1,60 +1,53 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <?php
+        echo "<h1>Variables</h1>";
 
-<?php
+        echo "<h2>Operation</h2>";
+        // int (with comment)
+        $x = 5 /* + 15 */ + 5;
+        $y = 10;
+        // printing variables.
+        echo $x . "<br>";
+        echo $y . "<br>";
+        echo $x + $y . "<br><br>";
 
-echo "<h1>Variables</h1>";
+        echo "<h2>Types</h2>";
+        $x = 5999;
+        $y = 10.1;
+        $z = true;
+        $my_array = array("A", "B", "C");
 
-echo "<h2>Operation</h2>";
-// int (with comment)
-$x = 5 /* + 15 */ + 5;
-$y = 10;
-// printing variables.
-echo $x . "<br>";
-echo $y . "<br>";
-echo $x + $y . "<br><br>";
+        echo "<h2>Return</h2>";
+        // Return type and value.
+        echo 'x: $x<br>';
+        var_dump($x);
+        echo '<br>';
+        var_dump($y);
+        echo '<br>';
+        var_dump($z);
+        echo '<br>';
+        var_dump($my_array);
+        echo "<br>";
+        $x = null;
+        echo "new x:" . $x . " (empty string because NULL)<br>";
+        var_dump($x);
+        echo '<br><br>';
 
-echo "<h2>Types</h2>";
-$x = 5999;
-$y = 10.1;
-$z = true;
-$my_array = array("A", "B", "C");
+        echo "<h2>Case</h2>";
 
-echo "<h2>Return</h2>";
-// Return type and value.
-echo 'x: $x<br>';
-var_dump($x);
-echo '<br>';
-var_dump($y);
-echo '<br>';
-var_dump($z);
-echo '<br>';
-var_dump($my_array);
-echo "<br>";
-$x = null;
-echo "new x:" . $x . " (empty string because NULL)<br>";
-var_dump($x);
-echo '<br><br>';
+        echo "<h3>Functions</h3>";
+        // functions are not case sensitive.
+        ECHO "command ECHO<br>";
+        echo "command echo<br><br>";
 
-
-echo "<h2>Case</h2>";
-
-echo "<h3>Functions</h3>";
-// functions are not case sensitive.
-ECHO "command ECHO<br>";
-echo "command echo<br><br>";
-
-echo "<h3>Variables</h3>";
-// variables are case sensitive.
-$color = "red";
-echo "color: " . $color . " (was set)<br>";
-echo "COLOR: " . $COLOR . " (not set) <br>";
-echo "color: $color in a single string<br>";
-
-
-?>
-
+        echo "<h3>Variables</h3>";
+        // variables are case sensitive.
+        $color = "red";
+        echo "color: " . $color . " (was set)<br>";
+        echo "COLOR: " . $COLOR . " (not set) <br>";
+        echo "color: $color in a single string<br>";
+    ?>
 </body>
 </html>
-
