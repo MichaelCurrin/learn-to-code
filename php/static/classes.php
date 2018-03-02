8751dc4f79cbfa2030576eb849d4400e07ddc8cb
MichaelCurrin <michael.currin@gmail.com>
2018-03-02 20:18:32 +0200

    refactor: Update classes.php

--- php/static/classes.php
+++ php/static/classes.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <?php
+        echo "<h1>Classes</h1>";
 
-<?php
+        class Car {
+            // Initialise Car class.
+            function Car() {
+                // On this instance of Car, set variable model to a string.
+                $this->model = "VW";
+            }
+        }
 
-echo "<h1>Classes</h1>";
-
-class Car {
-    function Car() {
-        $this->model = "VW";
-    }
-}
-
-$my_car = new Car();
-echo $my_car->model;
-
-?>
-
+        // Instantiate variable as instance of class.
+        $my_car = new Car();
+        // Show the value of the model variable on the instance.
+        echo $my_car->model;
+    ?>
 </body>
 </html>
-
