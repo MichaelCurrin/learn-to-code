04f4debbb63d5fb0cfc1a9486f822a3d1c68909e
MichaelCurrin <michael.currin@gmail.com>
2018-03-02 20:20:27 +0200

    refactor: Update constants.php

--- php/static/constants.php
+++ php/static/constants.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <?php
+        echo "<h1>Constants</h1>";
 
-<?php
+        // A constant is an identifier for a value which is fixed in the
+        // script. A valid constant starts with a letter or underscore, but
+        // not a $ sign.
 
-echo "<h1>Constants</h1>";
+        // The syntax is as follows:
+        // define(name, value, case-insensitive);
+        // Where the value in place of 'case-insensitive' is optional and may
+        // be `true` or `false` and defaults to false.
 
-// Constants cannot have $ sign and are automatically global.
+        define("GREETING", "Hello!");
+        echo GREETING . "<br>";
+        // Gives the literal word 'greeting'.
+        echo greeting . "<br>";
 
-// case insensitive defaults to false
-// define(value, value case-insensitive)
+        define("GREETING", "Goodbye!", true);
+        echo GREETING . "<br>";
+        echo greeting . "<br>";
 
-define("GREETING", "Hello!");
-echo GREETING . "<br>";
-// gives the literal word 'greeting'
-echo greeting . "<br>";
-
-define("GREETING", "Goodbye!", true);
-echo GREETING . "<br>";
-echo greeting . "<br>";
-
-
-function myFunct() {
-    echo GREETING;
-}
-myFunct()
-
-?>
-
+        // Unlike variables, constants are automatically global, so they can be
+        // referenced inside a function's scope.
+        function myFunct() {
+            echo GREETING;
+        }
+        myFunct()
+    ?>
 </body>
 </html>
