3bf9a7f2357ce0fb4d1ee1202f56300d4f4f2116
MichaelCurrin <michael.currin@gmail.com>
2018-03-02 20:20:07 +0200

    refactor: Update comments.php

--- php/static/comments.php
+++ php/static/comments.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html>
 <body>
+    <!-- Use of HTML heading outside of php code -->
+    <h1>Comments</h1>
 
-<!-- use of heading outside code -->
-<h1>Comments</h1>
+    <?php
 
-<?php
+        // This is a comment.
+        echo "// Single-line comment<br><br>";
 
-echo "// text<br>";
-echo "# text<br>";
-echo "/* text */<br>";
+        # This is also a comment.
+        echo "# Single-line comment<br><br>";
 
-// This is a comment.
-# This is a comment.
-/* 
-This is a multi-line comment.
-*/
+        /* This another a comment */
+        echo "/* Multi-line comment */<br><br>";
 
-?>
+        /*
+         * This is a good way to layout
+         * a multi-line comment.
+         */
 
+        // Below is a multi-line string enclosed within a a single pair of
+        // double-quotes.
+        echo "
+            /*<br>
+            * Multi-line comment<br>
+            */<br>
+        ";
+
+    ?>
 </body>
 </html>
