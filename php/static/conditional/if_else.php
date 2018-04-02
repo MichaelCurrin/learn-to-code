ce59e6b0c7a01ca7a621d1bf853a60ce9c7aa030
MichaelCurrin <michael.currin@gmail.com>
2018-04-02 21:43:23 +0200

    feature: Update if_else.php

--- php/static/conditional/if_else.php
+++ php/static/conditional/if_else.php
@@ -1,21 +1,42 @@
 <?php
 
-echo "<h1>Conditional statements</h1>";
+echo "<h1>Conditional statements</h1>\n";
 
-echo "Types of statements   <br>";
-echo "if...<br>";
-echo "if...else...<br>";
-echo "if...elseif...else<br>";
+echo "Types of statements   <br>\n";
+echo "if...<br>\n";
+echo "if...else...<br>\n";
+echo "if...elseif...else<br>\n\n";
 
 $x = 5;
 
-echo "Output:<br>";
+
+if ($x > 5)
+    echo "A<br>\n";
+
+if ($x > 5)
+    echo "A<br>\n";
+else
+    echo "B<br>\n";
+
+if ($x > 5)
+    echo "A<br>\n";
+elseif ($x > 2)
+    echo "B<br>\n";
+elseif ($x == 0 )
+    echo "C<br>\n";
+else
+    echo "D<br>\n";
+
+
+// Add curly brackets to execute multiple statements.
 if ($x > 5) {
-    echo "A";
-} elseif ($x > 2) {
-    echo "B";
+    echo "A1<br>\n";
+    echo "A2<br>\n";
 }
-else {
-    echo "C";
+elseif ($x > 2) {
+    echo "B1<br>\n";
+    echo "B2<br>\n";
 }
+else
+    echo "C<br>\n";
 
