a52830ae62013563a091dc5a1eaa6e42e63c13ee
MichaelCurrin <michael.currin@gmail.com>
2018-06-17 22:41:00 +0200

    feature: Create arrays.rb

---
+++ ruby/arrays.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+#
+# Arrays in Ruby.
+#
+# See https://www.tutorialspoint.com/ruby/ruby_arrays.htm
+
+my_array = ['a', 'b', 'c']
+
+print my_array
+#["a", "b", "c"]
+puts
+
+puts my_array
+#a
+#b
+#c
+puts
+
+puts my_array[1]
+#b
+puts
+
+digits = Array(0..9)
+print digits
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]=> nil
