638647e3e2f6dc64bbff38aaf7003bf5452d725f
Michael Currin <michael.currin@gmail.com>
2018-10-29 13:21:51 +0200

    feature: Create comments.js

---
+++ JavaScript/basics/comments.js
@@ -0,0 +1,12 @@
+// This is a single line comment.
+
+/* This is a
+   multiline comment */
+
+/** Documentation for functions looks like this **/
+function myFunct() {}
+
+/**
+ * Large sections of comments tend to
+ * look like this.
+ */
