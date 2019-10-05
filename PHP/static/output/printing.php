448846b7238382d0d0a2172b1a84184ce43844e3
Michael Currin <michael.currin@gmail.com>
2019-10-05 16:41:46 +0200

    feat: Update printing.php

--- PHP/static/output/printing.php
+++ PHP/static/output/printing.php
@@ -1,13 +1,31 @@
 <?php
 
+/** Printing **/
+
+// Link: https://www.w3schools.com/php/php_echo_print.asp
+
+// The `echo` and `print` commands can be used to output data. The main practical difference that
+// `echo` returns no value, while `print` returns 1, so can be used in expressions.
+
+// Previously echo was a statment and print a command.
+//   echo "Text";
+//   print("Text");
+// Now both work as statements and functions, with or without brackets.
+
 echo "<h1>Printing</h1><br>";
-// The `echo` and `print` commands can be used to output data.
-// `echo` returns no value.
-// `print` returns 1, so can be used in expressions.
+
 
 echo "Line 1<br>";
 print "Line 2<br>";
-echo print "Line 3<br><br>";
+echo print "Text";
+// => Text1
+
+// This will print the input but store 1.
+$a = print("Text");
+// => Text;
+echo $a;
+// => 1;
+
 
 $x = 5;
 
@@ -18,11 +36,12 @@ echo "x: $x<br>";
 
 // Concatenate values.
 
-// Seperate with either a comma.
+// echo accepts multiple parameters, though this is rare.
+// A usecase is to seperate values with a comma.
 echo "A", $x, "B<br>";
 
-// Or a full-stop.
+// You can also use a full-stop.
 echo "A" . $x . "B<br>";
 
-// Though, `print`  will only handle a full-stop.
+// Though `print`  will handle a full-stop only as it takes one parameter.
 print "A" . $x . "B<br>";
