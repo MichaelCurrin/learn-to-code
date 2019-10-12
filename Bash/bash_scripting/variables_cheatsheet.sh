2c6f335598b582baeeddbda438b5b3fed3acb008
michaelcurrin <michael.currin@gmail.com>
2019-10-12 10:18:42 +0200

    feat: Create script

---
+++ Bash/bash_scripting/variables_cheatsheet.sh
@@ -0,0 +1,12 @@
+# This is a last resort in checks, as it specifies your default shell and not necessarily the current shell.
+echo $SHELL
+# /bin/bash
+
+echo $BASH
+# /bin/bash
+
+echo $BASH_VERSION
+# 4.4.12(1)-release
+
+echo $ZSH_VERSION
+#
