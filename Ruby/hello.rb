31c5210e7cabb3225ffe6f180d275e4b32f549da
michaelcurrin <michael.currin@gmail.com>
2019-12-04 14:52:13 +0200

    feat: Create hello.rb

---
+++ Ruby/hello.rb
@@ -0,0 +1,10 @@
+# Copied from: https://www.ruby-lang.org/en/
+
+# The famous Hello World Program is trivial in Ruby.
+#
+# Superfluous:
+# * A "main" method
+# * Newline
+# * Semicolons
+
+puts "Hello World!"
