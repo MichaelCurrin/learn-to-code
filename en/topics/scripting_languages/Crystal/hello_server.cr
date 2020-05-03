b7550891e42ebf501e38559fdec958d9ec93c260
michaelcurrin <michael.currin@gmail.com>
2020-05-03 22:58:28 +0200

    Create hello_server.cr

---
+++ en/topics/scripting_languages/Crystal/hello_server.cr
@@ -0,0 +1,13 @@
+# Hello world server
+#
+# Copied from https://crystal-lang.org/reference/getting_started/http_server.html
+require "http/server"
+
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello world! The time is #{Time.local}"
+end
+
+address = server.bind_tcp 8080
+puts "Listening on http://#{address}"
+server.listen
