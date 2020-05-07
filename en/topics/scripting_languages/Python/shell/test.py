f8cb90c3f472a11a437f0464b8fd2c957c978898
michaelcurrin <michael.currin@gmail.com>
2020-05-07 10:01:55 +0200

    Update and rename Scripting languages/Python/tutorials/shell_test.py to en/topics/scripting_languages/Python/shell/test.py

--- en/topics/scripting_languages/Python/shell/test.py
+++ en/topics/scripting_languages/Python/shell/test.py
@@ -1,12 +1,14 @@
 import shlex
 import subprocess
 
+
 # run
 
 cmd = 'echo "Here are your files:" ; ls'
 
-
-# This didn't work properly - only first command was executed. Perhaps it was split wrong. Or only one command is allowed even with separator.
+# FIXME
+# This didn't work properly - only first command was executed. 
+# Perhaps it was split wrong. Or only one command is allowed even with separator.
 
 cmd_list = shlex.split(cmd)
 print(cmd_list)
